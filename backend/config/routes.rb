dcbc2dc5583fd93b99df2866b5b8a58a4cfaa330
skr1pt1k <skr1pt1k1997@gmail.com>
2019-02-26 15:25:29 +0200

    fix

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   resources :users
   resources :user_confirmations, only: :create
   resources :authentication, path: 'auth', only: :create
-  resources :conversations, only: %i[index create] do
-    resources :messages, only: %i[index create]
-  end
   resources :password_resets, only: :create
   resources :password_updates, only: :create
   resources :conversations, only: %i[index create] do
