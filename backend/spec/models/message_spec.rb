85b4663f6584fcae5aba83c46550918c6ec6c27a
skr1pt1k <skr1pt1k1997@gmail.com>
2019-02-26 13:53:55 +0200

    rspec for msg + conversation

--- backend/spec/models/message_spec.rb
+++ backend/spec/models/message_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  it { is_expected.to(validate_presence_of(:body)) }
+  describe 'associations' do
+    it { is_expected.to(belong_to(:conversation)) }
+    it { is_expected.to(belong_to(:user)) }
+  end
 
-  it { is_expected.to(validate_presence_of(:conversation_id)) }
-
-  it { is_expected.to(validate_presence_of(:user_id)) }
-
-  it { is_expected.to(belong_to(:conversation)) }
-
-  it { is_expected.to(belong_to(:user)) }
+  describe 'validations' do
+    it { is_expected.to(validate_presence_of(:body)) }
+    it { is_expected.to(validate_presence_of(:conversation_id)) }
+    it { is_expected.to(validate_presence_of(:user_id)) }
+  end
 end
