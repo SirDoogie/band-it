72e2b5cbb3ffa1d394a4cc97334a01afb9b13fd1
skr1pt1k <skr1pt1k1997@gmail.com>
2019-02-26 13:08:29 +0200

    rspec for message + conversations

--- backend/spec/models/message_spec.rb
+++ backend/spec/models/message_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Message, type: :model do
   it { is_expected.to(validate_presence_of(:conversation_id)) }
 
   it { is_expected.to(validate_presence_of(:user_id)) }
+
+  it { is_expected.to(belong_to(:conversation)) }
+
+  it { is_expected.to(belong_to(:user)) }
 end
