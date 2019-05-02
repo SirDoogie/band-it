9df2ba4b51fcb3b04c639606185a2cb0709af514
skr1pt1k <skr1pt1k1997@gmail.com>
2019-05-02 17:06:20 +0300

    fix reload issue (when always go to feed)

--- frontend/src/_reducers/auth.reducer.js
+++ frontend/src/_reducers/auth.reducer.js
@@ -1,46 +1,49 @@
 import { authConstants } from '_constants'
 
-const initialState = { loggedIn: false, currentUser: {} }
+const initialState = { loggedIn: false, currentUser: {}, loading: true }
 
 export function auth(state = initialState, action) {
   switch (action.type) {
-  case authConstants.LOGIN_SUCCESS:
-    return {
-      ...state,
-      loggedIn: true
-    }
-  case authConstants.LOGIN_FAILURE:
-    return {
-      ...state,
-      error: action.error
-    }
-  case authConstants.LOGOUT:
-    return {
-      ...state,
-      loggedIn: false
-    }
-  case authConstants.CURRENT_USER_REQUEST:
-    return {
-      ...state,
-      currentUser: {
-        loading: true
+    case authConstants.LOGIN_SUCCESS:
+      return {
+        ...state,
+        loggedIn: true
       }
-    }
-  case authConstants.CURRENT_USER_SUCCESS:
-    return {
-      ...state,
-      loggedIn: true,
-      currentUser: action.user
-    }
-  case authConstants.CURRENT_USER_FAILURE:
-    return {
-      ...state,
-      loggedIn: false,
-      currentUser: {
+    case authConstants.LOGIN_FAILURE:
+      return {
+        ...state,
         error: action.error
       }
-    }
-  default:
-    return state
+    case authConstants.LOGOUT:
+      return {
+        ...state,
+        loggedIn: false
+      }
+    case authConstants.CURRENT_USER_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        currentUser: {
+          loading: true
+        }
+      }
+    case authConstants.CURRENT_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loggedIn: true,
+        currentUser: action.user
+      }
+    case authConstants.CURRENT_USER_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        loggedIn: false,
+        currentUser: {
+          error: action.error
+        }
+      }
+    default:
+      return state
   }
 }
